fd837cad8cdf7a4dcc6710c86c6a3ff9f48f5723
pujit <ontikommupujit@gmail.com>
2026-07-07 14:56:21 -0400

    percentage

---
+++ percentage.py
@@ -0,0 +1,7 @@
+math=int(input("enter the marks of math"))
+science=int(input("enter the marks of science"))
+history=int(input("enter the marks of history"))
+sum=math+science+history
+print("the sum of the subjects are",sum)
+percentage=(sum/300)*100
+print("the percentage score is",percentage)
