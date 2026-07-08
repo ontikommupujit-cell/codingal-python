7a681761a48456fd735398da90dc13a0e92c3665
pujit <ontikommupujit@gmail.com>
2026-07-08 14:56:43 -0400

    greater or less than

---
+++ num.py
@@ -0,0 +1,8 @@
+x=int(input('enter the number'))
+if(x>10):
+    print("the value of x is greater than 10")
+    print('I am inside the if block')
+else:
+    print("I am smaller than 10")
+    print("i am in the else block")
+print("im not in the if or else block")
