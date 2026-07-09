11f8377f50719c57e27d0b9eeb8c9372bc16dde7
pujit <ontikommupujit@gmail.com>
2026-07-09 14:49:03 -0400

    not

---
+++ not.py
@@ -0,0 +1,15 @@
+a=9
+b=3
+c=3
+print(not(a==b))
+print(not(b==c))
+a="python"
+b="python"
+if not(a==b):
+    print("both are different")
+else:
+    print('both are same')
+a=6
+b=7
+if not((a==1)==(b==7)):
+    print('hello')
