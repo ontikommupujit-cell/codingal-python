fc68a9b5e088254fa9fb78ea5e7cff09e51cecb5
pujit <ontikommupujit@gmail.com>
2026-07-09 14:26:24 -0400

    operater or

---
+++ operator2.py
@@ -0,0 +1,11 @@
+a=7
+b=-5
+c=0
+if a>0 or b>0:
+    print('one of them is greater than 0')
+else:
+    print('none of them is greater than zero')
+if b>0 or c>0:
+    print('one of them is greater than 0')
+else:
+    print('none of them is greater than zero')
