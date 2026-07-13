bfac72c64c9746027c26237c73a1142b933f9e00
pujit <ontikommupujit@gmail.com>
2026-07-13 14:35:58 -0400

    mean

---
+++ mean value.py
@@ -0,0 +1,10 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+sum=mean1*total_number
+print(sum)
+num2=sum-((wrong_number)-correct_number)
+print(num2)
+mean2=num2/total_number
+print(mean2)
