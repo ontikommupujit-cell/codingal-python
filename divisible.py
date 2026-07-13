6219daac55868ee70828e2132b5df9cbd18fb5cd
pujit <ontikommupujit@gmail.com>
2026-07-13 14:24:37 -0400

    divisible

---
+++ divisible.py
@@ -0,0 +1,7 @@
+numerator=int(input('enter the numerator:'))
+denominator=int(input('enter the denominator:'))
+if numerator%denominator==0:
+    print(str(numerator)+"is divisible by"+str(denominator))
+else:
+    print(str(numerator)+'is not divisible by'+str(denominator))
+
