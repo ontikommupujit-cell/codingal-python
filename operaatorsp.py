c6a1f1c97e007397068999ccc2476419fa2036a2
pujit <ontikommupujit@gmail.com>
2026-07-13 14:15:20 -0400

    operaterp

---
+++ operaatorsp.py
@@ -0,0 +1,15 @@
+a=90
+b=54
+c=67
+d=64
+e=7
+z=0
+z=(a+b)*c/d-e;
+print("the value of z is",z)
+name="peter"
+age=20
+if name=="john" or name=="jack" and age>=21:
+    print('hello world')
+else:
+    print('goodbye world')
+
