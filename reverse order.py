67125fdf633b9722644302e4c0504e6d8a5978d8
pujit <ontikommupujit@gmail.com>
2026-07-15 14:50:24 -0400

    reverse order

---
+++ reverse order.py
@@ -0,0 +1,4 @@
+a=(int(input('enter a number')))
+print('number from{0}to{1}r'.format(a,1))
+for i in range (a,0,-1):
+    print(i)
---
+++ reverse string2.py
@@ -0,0 +1,5 @@
+a=input("enter a string")
+string2=a[::-1]
+
+print('the original string =',a)
+print("the reversed string =",string2)
