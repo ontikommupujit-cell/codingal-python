42670a8acdc48a82f37aafc4402d5aca2dde49d0
pujit <ontikommupujit@gmail.com>
2026-07-20 14:47:16 -0400

    dimbond

---
+++ dimond.py
@@ -0,0 +1,26 @@
+rowsize=int(input('please enter a number of rows'))
+if rowsize %2==0:
+    halfrow=int(rowsize/2)
+else:
+    halfrow=int(rowsize/2)+1
+space=halfrow-1
+for i in range(1,halfrow+1):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space-1
+    num=1
+    for j in range(2*i-1):
+        print(end=str(num))
+        num=num+1
+    print()
+space=1
+#lowerhalf code
+for i in range(1,halfrow):
+    for j in range (1,space+1):
+        print(end=" ")
+    space=space+1
+    num=1
+    for j in range(1,2*(halfrow-i)):
+        print(end=str(num))
+        num=num+1
+    print()
