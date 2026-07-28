1e950c67627001b2c1eb9fb6e8b2d0adc0062f75
pujit <ontikommupujit@gmail.com>
2026-07-28 14:35:30 -0400

    gcd

---
+++ calculator.py
@@ -0,0 +1,7 @@
+import math
+print('the floor and ceiling value of 5.6 is'+str(math.ceil(5.6))+","+str(math.floor(5.6)))
+x=10
+y=-15
+print('the value of x after copying the sign from y is'+str(math.copysign(x,y)))
+print('the absolute value of -40 and 40 is'+str(math.fabs(-40))+','+str(math.fabs(40)))
+print('the gcd of 14 and 7 is'+str(math.gcd(14,7)))
