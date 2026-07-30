524431450dbf1484fdd716f1826a658ac5cdca05
pujit <ontikommupujit@gmail.com>
2026-07-30 14:17:22 -0400

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+empty_list=[]
+print()
+#a list of numbers
+numbers=[1,2,3,4,5,6]
+print(numbers)
+#operater list
+triples=[1,2,3]*3
+print(triples)
+#reverse the list
+list=[100,200,300,400,500]
+list=list[::-1]
+print(list)
