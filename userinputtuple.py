dc0a23a2c6e05bf4c3e1eb81ad4e2c71f6119b48
pujit <ontikommupujit@gmail.com>
2026-08-02 21:52:40 -0400

    tuples

---
+++ userinputtuple.py
@@ -0,0 +1,14 @@
+numbers=()
+
+numbers=(
+    int(input("Enter number 1:")),
+    int(input("Enter number 2:")),
+    int(input("Enter number 3:")),
+    int(input("Enter number 4:")),
+    int(input("Enter number 5:"))
+)
+
+print(numbers)
+print(numbers[0])
+print(numbers[-1])
+print(numbers[1:4])
