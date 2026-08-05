bb50a26839acfb5ca6834314f2ddf45b1b6660fc
pujit <ontikommupujit@gmail.com>
2026-08-05 13:53:21 -0400

    month2

---
+++ months2.py
@@ -0,0 +1,10 @@
+import calendar
+
+print("===== MONTH NAMES PROGRAM =====")
+print("Displaying all the months:\n")
+
+for i in range(1,13):
+    print(i,".",calendar.month_name[i])
+
+print("\nAll month names displayed successfully.")
+print("===== END OF PROGRAM =====")
